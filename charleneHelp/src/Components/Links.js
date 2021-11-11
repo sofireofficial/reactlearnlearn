7819f66edb84781b0e2e51f69ff5eac24f2c4f17
SofireOfficial <soph.johns247@gmail.com>
2021-11-11 13:47:37 +0000

    Tuesday changes with Charlene

---
+++ charleneHelp/src/Components/Links.js
@@ -0,0 +1,9 @@
+
+const Links =
+
+<a href="https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/#basic-javascript" target="blank"><strong>&gt; freecodecamp</strong> - General</a>
+<a href="https://www.freecodecamp.org/learn/responsive-web-design/" target="blank"><strong>&gt; freecodecamp</strong> - Responsive Web Design</a>
+<a href="https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/" target="blank"><strong>&gt; freecodecamp</strong> - JS Algorithms &amp; Data Structures</a>
+<a href="https://www.freecodecamp.org/news/learn-the-basics-of-git-in-under-10-minutes-da548267cc91/?__s=w0w1atafcond51k7bwt9" target="blank">&gt; Learn the basics of <strong>Git</strong>. 10 Mins!</a>
+
+export default Links
