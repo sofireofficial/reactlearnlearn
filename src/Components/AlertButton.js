a8bf14be5dce896444e8c1f43ecf66d8003d2c70
SofireOfficial <soph.johns247@gmail.com>
2022-01-04 10:05:52 +0000

    update

--- src/Components/AlertButton.js
+++ src/Components/AlertButton.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function dare() {
-  alert("Aaaaaaaaaaaaaaaaaaaahhhhhhhhhhhh you clicked! You really clicked? You still chose to click the button, after I told you not to! Mmh Mmh Mmh... *Shaking my head*");
+  alert("Aaaaaaaaaaaaaaaaaaaaaaaaahhhhhhhhhhhh you clicked! You really clicked? You still chose to click the button, after I told you not to! Mmh Mmh Mmh... *Shaking my head*");
 }
 
 const AlertButton = (props) => {
